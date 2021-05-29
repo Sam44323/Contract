f6b697fb178434bc7e1af038272558fda6280ce3
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-30 00:18:13 +0530

    add: comments added to compile.js

--- compile.js
+++ compile.js
@@ -1,3 +1,7 @@
+/*
+We are using path instead of require, as require instantly reads the code and as here, we are javascript, if we require sol codes then we will get an error
+*/
+
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
