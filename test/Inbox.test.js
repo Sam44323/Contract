ed3e99b2c2fc33dcb78c20a3e946effdee0f759c
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-30 01:31:17 +0530

    add: created a web3 instance

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,6 @@
-// contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3"); // Web3 is a constructor function, so we used upper-case
+
+// creating a web3 instance with a provider
+const web3 = new Web3(ganache.provider());
