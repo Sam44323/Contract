33a19609a7ad6f90d1e25ff2bdb6ff318468846c
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-30 02:07:42 +0530

    add: code for fetching test accounts from gananche

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,3 +4,17 @@ const Web3 = require("web3"); // Web3 is a constructor function, so we used uppe
 
 // creating a web3 instance with a provider
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  // get a list of all accounts from the ganache local network
+
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+
+  // use one of the those accounts to deploy the contract
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {});
+});
