18b86eff5db6240b1879aaaae7507ba13c54aa6c
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-30 10:53:54 +0530

    add: logic added for deploying a contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,21 +1,32 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3"); // Web3 is a constructor function, so we used upper-case
-
+const { interface, bytecode } = require("../compile");
 // creating a web3 instance with a provider
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
-  // get a list of all accounts from the ganache local network
+  // get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // use one of the those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
+    });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
     console.log(accounts);
+    console.log(inbox);
   });
 });
