42d0de8f4b4d88ea25c33afbf58bfbb202df2a91
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-01 22:13:35 +0530

    add: custom provider setup done

--- deploy.js
+++ deploy.js
@@ -1,1 +1,11 @@
-// deploy code will go here
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+// configuring a custom provider for connecting to an external API and unlocking an account
+const provider = new HDWalletProvider(
+  "void atom chimney where army mystery garage must tiny swear release recall",
+  "https://rinkeby.infura.io/v3/c65de9dd9e6a4d2cb17c756fb3f6a446"
+);
+
+const web3 = new Web3(provider);
