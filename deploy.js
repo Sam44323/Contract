7baef7b3d3f7e34dad2d539aadda17e8caccf32f
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-02 00:43:04 +0530

    refactor: edited the .gitignore file

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ const { interface, bytecode } = require("./compile");
 
 // configuring a custom provider for connecting to an external API and unlocking an account for deploy
 const provider = new HDWalletProvider(
-  "void atom chimney where army mystery garage must tiny swear release recall",
+  process.env.DATA,
   "https://rinkeby.infura.io/v3/c65de9dd9e6a4d2cb17c756fb3f6a446"
 );
 
